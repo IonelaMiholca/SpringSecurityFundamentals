ccaa9ee4de3d0f7fa53d5a976dba036249e0c652
ionela.miholca <ionela_maria92@yahoo.com>
2023-03-09 09:19:18 +0200

    Create new user&password with HTTP Basic auth

---
+++ src/main/java/com/spring/security/example/SpringSecurityFundamentals/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.spring.security.example.SpringSecurityFundamentals.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //provides the ability to create new users and update existing ones
+        var userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("ionela")
+                .password("test123")
+                // read/write/delete
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(user);
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
