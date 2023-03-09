1709cb6b4e04a2742ef6a46d8ef8bcc0c0f56eaf
ionela.miholca <ionela_maria92@yahoo.com>
2023-03-09 08:58:24 +0200

    first commit

---
+++ src/test/java/com/spring/security/example/SpringSecurityFundamentals/SpringSecurityFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.example.SpringSecurityFundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
